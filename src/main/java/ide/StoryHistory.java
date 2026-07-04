3a6d62ef67d69e3452d2506f14101064903bddf6
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-07-05 00:16:07 +0300

    initial commit

---
+++ src/main/java/ide/StoryHistory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ide;
+
+/**
+ *
+ * @author rash4
+ */
+public class StoryHistory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
